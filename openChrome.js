0c1888a15fd6b67124cb34e963201e26cf1b1036
EvgeniaDingilska <dingilska@gmail.com>
2023-12-14 16:25:08 +0100

    "open side end verify is the correct one"

--- openChrome.js
+++ openChrome.js
@@ -5,9 +5,16 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
 
   // Replace 'https://example.com' with your desired URL
-  await page.goto('https://example.com');
-
-  // Optional: Add more interactions or actions here
+  await page.goto('http://localhost:3000');
+  
+  // Verify content on the page
+  const pageTitle = await page.title();
+  if (pageTitle.includes('W3.CSS Template')) {
+    console.log('Content verification passed!');
+  } else {
+    console.error('Content verification failed.');
+    process.exit(1); // Exit with a non-zero status code to indicate failure
+  }
 
   // Close the browser
   await browser.close();
